4eca7a01b39bb3b2240b3fbafc7321231e2e6712
skennetho <thrkd613@naver.com>
2022-08-01 09:23:33 +0900

    백준 2557 풀었음.

---
+++ StartersGroupStudy_August/Baekjoon/BJ_2557.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Program
+{
+    class BJ_2557
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
