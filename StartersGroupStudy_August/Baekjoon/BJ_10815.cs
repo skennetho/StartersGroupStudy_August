bb86233204b023f2cdb11a74e0f636b3f3ca4a8a
skennetho <thrkd613@naver.com>
2022-09-01 10:32:18 +0900

    백준 10815, 1764 풀었음

---
+++ StartersGroupStudy_August/Baekjoon/BJ_10815.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace StartersGroupStudy_August.Baekjoon
+{
+    internal class BJ_10815
+    {
+        public static void Mai()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arrN = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int m = int.Parse(Console.ReadLine());
+            int[] arrM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            HashSet<int> set = new HashSet<int>(arrN);
+
+            foreach (int i in arrM)
+            {
+                if (set.Contains(i))
+                {
+                    sb.Append(1 + " ");
+                }
+                else
+                {
+                    sb.Append(0 + " ");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+
+        void code1()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arrN = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int m = int.Parse(Console.ReadLine());
+            int[] arrM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                map.Add(arrN[i], 1);
+            }
+
+            foreach (int i in arrM)
+            {
+                if (map.ContainsKey(i))
+                {
+                    sb.Append(1 + " ");
+                }
+                else
+                {
+                    sb.Append(0 + " ");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StartersGroupStudy_August/Baekjoon/BJ_1764.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace StartersGroupStudy_August.Baekjoon
+{
+    internal class BJ_1764
+    {
+        public static void Mai()
+        {
+            int[] nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nm[0];
+            int m = nm[1];
+
+            HashSet<string> 듣도못한사람들 = new HashSet<string>();
+            List<string> 듣보잡 = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                듣도못한사람들.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                string 보도못한사람 = Console.ReadLine();
+                if (듣도못한사람들.Contains(보도못한사람))
+                {
+                    듣보잡.Add(보도못한사람);
+                }
+            }
+            듣보잡.Sort();
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(듣보잡.Count + "");
+            듣보잡.ForEach(name => sb.AppendLine(name));
+            
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
